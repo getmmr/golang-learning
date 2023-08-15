573896b60ede09efa65ecf111a883ebb4d76c853
ch <chgmworkmail@gmail.com>
2023-08-15 16:32:18 +0800

    Changes to be committed:   modified:   httpserver/http_02.go

--- httpserver/http_02.go
+++ httpserver/http_02.go
@@ -7,8 +7,12 @@ import (
 )
 
 func Test2() {
-	http.HandleFunc("/hello", HandleHello)
-	err := http.ListenAndServe(":8000", nil)
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/hello", HandleHello)
+
+	serverMux.HandleFunc("/post", HandlePost)
+
+	err := http.ListenAndServe(":8000", serverMux)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error:%v\n", err)
 	}
@@ -18,4 +22,19 @@ func HandleHello(writer http.ResponseWriter, req *http.Request) {
 	clientInfo := fmt.Sprintf("HandleHello: %s %s %s\n", req.RemoteAddr, req.Method, req.RequestURI)
 	fmt.Printf(clientInfo)
 	_, _ = fmt.Fprintf(writer, clientInfo)
+}
+
+func HandlePost(writer http.ResponseWriter, req *http.Request) {
+	clientInfo := fmt.Sprintf("HandlePost: %s %s %s \n", req.RemoteAddr, req.Method, req.RequestURI)
+	fmt.Printf(clientInfo)
+
+	err := req.ParseForm()
+	if err != nil {
+		http.Error(writer, fmt.Sprintf("error: %v\n", err), http.StatusInternalServerError)
+		return
+	}
+	formFields := req.PostForm
+	fmt.Printf("Form fields: %v\n", formFields)
+	writer.WriteHeader(http.StatusOK)
+	_, _ = writer.Write([]byte("提交成功\n"))
 }
