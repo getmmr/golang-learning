0798aff6c4e90d84cb137e4a7878c749c66eb215
ch <chgmworkmail@gmail.com>
2023-08-15 10:11:31 +0800

    new file:   httpserver/http1.go

---
+++ httpserver/http1.go
@@ -0,0 +1,22 @@
+package httpserver
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HttpHandle struct {
+}
+
+func (h *HttpHandle) ServeHTTP(respWriter http.ResponseWriter, req *http.Request) {
+	fmt.Printf("ClientAddr: %s\n", req.Host)
+
+	respWriter.Header().Add("Key-Hello", "Value-World")
+	respWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	respWriter.WriteHeader(200)
+
+	clientInfo := fmt.Sprintf("ServeHTTP: %s %s %s\n", req.RemoteAddr, req.Method, req.RequestURI)
+
+	_, _ = respWriter.Write([]byte(clientInfo))
+}
